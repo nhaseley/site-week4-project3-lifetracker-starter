9736f4717126b0c89d5b5ba4107804313260a660
Nya Haseley-Ayende <nhaseleyayende@salesforce.com>
2023-06-27 11:15:50 -0700

    project now set up

---
+++ lifetracker-api/config.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
+// const IS_TESTING = process.env.NODE_ENV === "test"
+
+
+// Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+    const dbBcryptWorkFactor = parseInt(process.env.BCRYPTWORKFACTOR) || 13;
+    const dbUser = process.env.DATABASE_USER || "postgres"
+    const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : "postgres"
+    const dbHost = process.env.DATABASE_HOST || "localhost"
+    const dbPort = process.env.DATABASE_PORT || 5432
+    const dbTestName = process.env.DATABASE_TEST_NAME || "lifetracker_test"
+    const dbProdName = process.env.DATABASE_NAME || "lifetracker"
+    const dbName = process.env.NODE_ENV === "test" ? dbTestName : dbProdName
+  
+    return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+  }
